a88ddd49f961682d8d3f563aff72080ddd51128c
Alexandr Rybakov <38357811+rybakooov@users.noreply.github.com>
2020-03-12 18:41:30 +0300

    push

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,6 +15,7 @@ $(document).ready(function(){
   /* табы */
   $('.tabs-wrap').each(function(){
     slideTabs($(this));
+    $(this).find('.blocks').css('height', $(this).find('.blocks__block.active').height());
   })
 
 
@@ -29,6 +30,8 @@ $(document).ready(function(){
     _this.find($('.blocks__block.active')).removeClass('active').fadeToggle();
     setTimeout(() => {
       _this.find($(`.blocks__block[data-tabs="${$(this).attr('data-tabs')}"]`)).addClass('active').fadeToggle();
+      
+      _this.find('.blocks').css('height', _this.find('.blocks__block.active').height());
     }, 400)
 
 
@@ -51,13 +54,11 @@ $(document).ready(function(){
 
 
   /*faq item */
-  $(document).on('click', '.faq-item-top', function(){
-    $(this).closest('.faq-item').toggleClass('active');
-    $(this).closest('.faq-item').find('.faq-item-main').slideToggle();
+  $(document).on('click', '.faq-item-wrap', function(){
+    $(this).find('.faq-item').toggleClass('active');
+    $(this).find('.faq-item').find('.faq-item-main').slideToggle();
   })
   /*faq item end */
-
-
 
 
 
@@ -93,6 +94,8 @@ $(document).ready(function(){
     var j = 0;
     var NumberOfFiles = file.length;
     var sumFiles = 0;
+    file.value = '';
+    console.log(file);
     for(j; j< NumberOfFiles;j++){
       sumFiles+=file[j].size;
     }
@@ -174,11 +177,13 @@ $(document).ready(function(){
       if (input.val() == '') {
         input.removeClass('input-valid');
         input.addClass('input-error');
+        input.siblings('.form-label__alert').html('Это обязательное поле');
         return false;
       } else {
         if (input.val().length != 18) {
           input.removeClass('input-valid');
           input.addClass('input-error');
+          input.siblings('.form-label__alert').html('Телефон должен содержать 10 цифр');
           return false;
         } else {
           input.removeClass('input-error');
@@ -200,4 +205,99 @@ $(document).ready(function(){
   }
   
   /* form mask + validate end */
+
+
+
+  /* hover image */
+
+  var imagesHover = document.querySelectorAll('.result-item-imgbox');
+  for (index = 0; index < imagesHover.length; index++) {
+    img = imagesHover[index];
+    img.addEventListener('mousemove', mousemoveImgHandler);
+    img.addEventListener('mouseleave', mouseleaveImgHandler);
+  }
+
+  function mousemoveImgHandler(e){
+    let x1 = (-(200 - e.offsetX) * 0.05),
+        y1 = (-(200 - e.offsetY) * 0.05);
+    $(this).attr('style', 'transform: translate(' + x1 + 'px, ' + y1 + 'px)');
+  }
+
+  function mouseleaveImgHandler (){
+    $(this).attr('style', '');
+  }
+
+  /* hover image end */
+
+  
+  /* header position */ 
+
+  function moveHeader (){
+    $('.header__burger').css('margin-right', Math.round($('.banner-main-woman img').width() * 0.74))
+    $('.header-gray').css('width', $('.header-overlay').width() - $('.header-info__phone').offset().left + 80)
+  }
+
+  $('.banner-main-woman img').on('load', function() {
+    
+    moveHeader();
+  });
+
+  $(window).resize(function(){
+    moveHeader();
+  })
+
+  /* header position end */
+
+
+
+  /* burger + menu */ 
+  var flagAnimated = 0;
+  $(document).on('click', '.header__burger', function(){
+    if (flagAnimated == 1) {
+      return false
+    }
+    flagAnimated = 1;
+    
+    $(this).toggleClass('active');
+    
+    if($(this).hasClass('active')){
+      $('.header-overlay').toggle(0 , function(){
+        $('.header-overlay').toggleClass('active');
+      });
+    } else {
+      $('.header-overlay').toggleClass('active');
+      setTimeout(function(){
+        $('.header-overlay').toggle();
+      }, 500);
+    }
+    setTimeout(function(){
+      flagAnimated = 0;
+    }, 500)
+
+
+  })
+
+
+  $(document).on('click', '.header-overlay', function(e){
+    if (e.target !== this)
+      return;
+      
+      
+    $('.header__burger').toggleClass('active');
+    $('.header-overlay').toggleClass('active');
+    setTimeout(function(){
+      $('.header-overlay').toggle();
+    }, 500)
+  })
+
+  /* burger + menu end */ 
+
+
+
 });
+
+
+
+
+
+
